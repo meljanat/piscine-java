7cb35e5356b7fb61cd74ba5bcf296d57c49f2198
Mouad El-Janati <mdjanati,mj@gmail.com>
2025-09-23 17:05:39 +0100

    Palindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,5 +1,8 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
         s = s.toLowerCase();
         String firstHalf = s.substring(0, s.length() / 2);
         String secondHalf = s.substring((s.length() + 1) / 2);
