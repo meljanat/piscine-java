46b4efa47444a134e7a7362ae6d939bf098c6efe
Mouad El-Janati <mdjanati,mj@gmail.com>
2025-09-23 17:15:56 +0100

    ComputeArray

---
+++ ComputeArray/ComputeArray.java
@@ -0,0 +1,12 @@
+public class ComputeArray {
+    public static int[] computeArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 3 == 0) {
+                array[i] = array[i] * 5;
+            } else if (array[i] % 3 == 1) {
+                array[i] = array[i] + 7;
+            }
+        }
+        return array;
+    }
+}
