793a3e90c23090d55fbe8313ad05c1252b761bf5
meljanat <mdjanati.mj@gmail.com>
2025-09-24 15:09:05 +0100

    DoOp

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -1,6 +1,6 @@
 public class DoOp {
     public static String operate(String[] args) {
-        if (args == null || args.length != 3) {
+        if (args == null) {
             return "it depend on the input.";
         }
         int first_op = Integer.parseInt(args[0]);
