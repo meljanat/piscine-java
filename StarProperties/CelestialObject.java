24718ef3f4ea2c1f744faf6703601564662605ce
meljanat <mdjanati.mj@gmail.com>
2025-09-24 16:57:38 +0100

    CelestialObject

--- StarProperties/CelestialObject.java
+++ StarProperties/CelestialObject.java
@@ -3,11 +3,4 @@ public class CelestialObject {
     public Double x;
     public Double y;
     public Double z;
-
-    CelestialObject() {
-        this.name = null;
-        this.x = 0.0;
-        this.y = 0.;
-        this.z = .0;
-    }
 }
