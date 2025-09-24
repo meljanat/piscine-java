6ef510c0dde3e4e530d16cebb00a4ad547fc47f5
meljanat <mdjanati.mj@gmail.com>
2025-09-24 17:15:20 +0100

    CelestialObject

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,52 @@
+public class CelestialObject {
+    public String name;
+    public double x;
+    public double y;
+    public double z;
+
+    public CelestialObject() {
+        this.name = "Soleil";
+        this.x = 0.0;
+        this.y = 0.;
+        this.z = .0;
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+}
