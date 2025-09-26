27dfe5dfcf8dead079602d3049aa76d57756632b
meljanat <mdjanat.mj@gmail.com>
2025-09-26 16:23:49 +0100

    CelestialObject

--- StarGalaxy/Galaxy.java
+++ StarGalaxy/Galaxy.java
@@ -1,10 +1,18 @@
 import java.util.*;
 
 public class Galaxy {
-    private List<CelestialObject> objects = new ArrayList<>();
+    private List<CelestialObject> celestialObjects = new ArrayList<>();
+
+    public Galaxy() {
+        this.celestialObjects = new ArrayList<>();
+    }
+
+    public List<CelestialObject> getCelestialObjects() {
+        return celestialObjects;
+    }
 
     public void addCelestialObject(CelestialObject obj) {
-        objects.add(obj);
+        this.celestialObjects.add(obj);
     }
 
     public Map<String, Integer> computeMassRepartition() {
@@ -13,7 +21,7 @@ public class Galaxy {
         repartition.put("Planet", 0);
         repartition.put("Other", 0);
 
-        for (CelestialObject obj : objects) {
+        for (CelestialObject obj : celestialObjects) {
             if (obj instanceof Star) {
                 repartition.put("Star", repartition.get("Star") + (int) obj.getMass());
             } else if (obj instanceof Planet) {
@@ -22,7 +30,6 @@ public class Galaxy {
                 repartition.put("Other", repartition.get("Other") + (int) obj.getMass());
             }
         }
-
         return repartition;
     }
 }
--- StarMass/Galaxy.java
+++ StarMass/Galaxy.java
@@ -1,8 +1,7 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Galaxy {
-    private List<CelestialObject> celestialObjects;
+    private List<CelestialObject> celestialObjects = new ArrayList<>();
 
     public Galaxy() {
         this.celestialObjects = new ArrayList<>();
@@ -14,5 +13,23 @@ public class Galaxy {
 
     public void addCelestialObject(CelestialObject obj) {
         this.celestialObjects.add(obj);
+    }
+
+    public Map<String, Integer> computeMassRepartition() {
+        Map<String, Integer> repartition = new HashMap<>();
+        repartition.put("Star", 0);
+        repartition.put("Planet", 0);
+        repartition.put("Other", 0);
+
+        for (CelestialObject obj : celestialObjects) {
+            if (obj instanceof Star) {
+                repartition.put("Star", repartition.get("Star") + (int) obj.getMass());
+            } else if (obj instanceof Planet) {
+                repartition.put("Planet", repartition.get("Planet") + (int) obj.getMass());
+            } else {
+                repartition.put("Other", repartition.get("Other") + (int) obj.getMass());
+            }
+        }
+        return repartition;
     }
 }
