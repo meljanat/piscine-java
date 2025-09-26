370e65c2618a5c093363698b7394e1441dbe08ea
meljanat <mdjanat.mj@gmail.com>
2025-09-26 16:08:49 +0100

    CelestialObject

--- StarMass/Planet.java
+++ StarMass/Planet.java
@@ -8,8 +8,8 @@ public class Planet extends CelestialObject {
         this.centerStar = new Star();
     }
 
-    public Planet(String name, double x, double y, double z, Star centerStar) {
-        super(name, x, y, z);
+    public Planet(String name, double x, double y, double z, Star centerStar, Integer mass) {
+        super(name, x, y, z, mass);
         this.centerStar = centerStar;
     }
 
