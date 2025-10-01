b3d11085d5fc31686d03127036367279e5d70323
meljanat <mdjanati.mj@gmail.com>
2025-10-01 11:40:04 +0100

    Character

---
+++ AdventureCharacter/Character.java
@@ -0,0 +1,43 @@
+public class Character {
+    final int maxHealth;
+    int currentHealth;
+    final String name;
+
+    public Character(String name, int maxHealth) {
+        this.name = name;
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        if (currentHealth > 0) {
+            return name + " : " + currentHealth + "/" + maxHealth;
+        } else {
+            return name + " : KO";
+        }
+    }
+
+    public void takeDamage(int damage) {
+        currentHealth -= damage;
+        if (currentHealth < 0) {
+            currentHealth = 0;
+        }
+    }
+
+    public void attack(Character target) {
+        target.takeDamage(9);
+    }
+}
