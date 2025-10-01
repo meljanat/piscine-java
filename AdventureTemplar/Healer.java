6f14638090c280385781fb3095fe8781dcbe3cea
meljanat <mdjanati.mj@gmail.com>
2025-10-01 13:38:33 +0100

    Templer

---
+++ AdventureTemplar/Healer.java
@@ -0,0 +1,5 @@
+public interface Healer {
+    void heal(Character target);
+
+    int getHealCapacity();
+}
---
+++ AdventureTemplar/Templar.java
@@ -0,0 +1,34 @@
+public class Templar extends Character implements Healer, Tank {
+    public final int healCapacity, shield;
+
+    public Templar(String name, int maxHealth, int healCapacity, int shield) {
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+        this.shield = shield;
+    }
+
+    @Override
+    public int getHealCapacity() {
+        return healCapacity;
+    }
+
+    @Override
+    public void heal(Character target) {
+        target.setHealth(target.getCurrentHealth() + healCapacity);
+    }
+
+    @Override
+    public int getShield() {
+        return shield;
+    }
+
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() == 0) {
+            return this.getName() + " has been beaten, even with its " + this.shield + " shield. So bad, it could heal "
+                    + this.healCapacity + " HP.";
+        }
+        return this.getName() + " is a strong Templar with " + this.getCurrentHealth() + " HP. It can heal "
+                + this.healCapacity + " HP and has a shield of " + this.shield + ".";
+    }
+}
