883ba061a37eeb403b5c907345db0536b6cbe270
meljanat <mdjanati.mj@gmail.com>
2025-10-02 14:23:56 +0100

    catch

--- AdventureCatch/Character.java
+++ AdventureCatch/Character.java
@@ -74,13 +74,13 @@ public abstract class Character {
         while (c1.getCurrentHealth() > 0 && c2.getCurrentHealth() > 0) {
             try {
                 c1.attack(c2);
-            } catch (DeadCharacterException e) {
+            } catch (Exception e) {
 
             }
             if (c2.getCurrentHealth() > 0) {
                 try {
                     c2.attack(c1);
-                } catch (DeadCharacterException e) {
+                } catch (Exception e) {
 
                 }
             }
