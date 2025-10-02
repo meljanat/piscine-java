910e1bda460666ca9b18a94c7ae694fc0dfd6f6d
meljanat <mdjanati.mj@gmail.com>
2025-10-02 16:07:15 +0100

    SortList

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -1,14 +1,15 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class SortList {
     public static List<Integer> sort(List<Integer> list) {
-        List<Integer> newList = List.copyOf(list);
+        List<Integer> newList = new ArrayList<>(list);
         newList.sort(Integer::compareTo);
         return newList;
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
-        List<Integer> newList = List.copyOf(list);
+        List<Integer> newList = new ArrayList<>(list);
         newList.sort((a, b) -> b.compareTo(a));
         return newList;
     }
