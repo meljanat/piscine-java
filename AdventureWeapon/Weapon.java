085185fe925faa81a4d64489894fe03db0bbe206
meljanat <mdjanati.mj@gmail.com>
2025-10-02 12:54:19 +0100

    Weapon

--- AdventureWeapon/Weapon.java
+++ AdventureWeapon/Weapon.java
@@ -1,6 +1,6 @@
 public class Weapon {
-    private String name;
-    private int damage;
+    private final String name;
+    private final int damage;
 
     public Weapon(String name, int damage) {
         this.name = name;
