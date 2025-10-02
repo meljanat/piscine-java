b0e24eab4225cd39227e04fca0c21cb6f583169b
meljanat <mdjanati.mj@gmail.com>
2025-10-02 14:55:53 +0100

    ListSearchIndex

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -3,11 +3,17 @@ import java.util.ArrayList;
 
 public class ListSearchIndex {
     public static Integer findLastIndex(List<Integer> list, Integer value) {
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
         int index = list.lastIndexOf(value);
         return index == -1 ? null : index;
     }
 
     public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
         int index = list.indexOf(value);
         return index == -1 ? null : index;
     }
