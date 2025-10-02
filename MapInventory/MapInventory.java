d647de2215ca407a7cbf017315a7b4f0cdcc246e
meljanat <mdjanati.mj@gmail.com>
2025-10-02 16:15:45 +0100

    MapInventory

---
+++ MapInventory/MapInventory.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MapInventory {
+    public static int getProductPrice(Map<String, Integer> inventory, String productId) {
+        if (inventory.get(productId) != null) {
+            return inventory.get(productId);
+        }
+        return -1;
+    }
+
+    public static List<String> getProductIdsByPrice(Map<String, Integer> inventory, int price) {
+        List<String> productIds = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            if (entry.getValue() == price) {
+                productIds.add(entry.getKey());
+            }
+        }
+        return productIds;
+    }
+}
