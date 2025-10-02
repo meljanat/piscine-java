2d2bb77c62c22a04a545141b24875a648d76c381
meljanat <mdjanati.mj@gmail.com>
2025-10-02 16:03:13 +0100

    SortList

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -1,7 +1,6 @@
 import java.util.List;
 
 public class SortList {
-
     public static List<Integer> sort(List<Integer> list) {
         List<Integer> newList = List.copyOf(list);
         newList.sort(Integer::compareTo);
