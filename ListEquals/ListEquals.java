f88b7246a12c95036592c6f8ae6d3a9737132fad
meljanat <mdjanati.mj@gmail.com>
2025-10-02 14:50:01 +0100

    ListEquals

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
-        if (list1.size() != list2.size()) {
+        if (list1 == null || list2 == null || list1.size() != list2.size()) {
             return false;
         }
         for (int i = 0; i < list1.size(); i++) {
