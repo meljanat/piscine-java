402e90f9f97cbc007ba3b569f61dca4ddd01f858
meljanat <mdjanati.mj@gmail.com>
2025-10-07 12:06:05 +0100

    ParseDate

--- ParseDate/ParseDate.java
+++ ParseDate/ParseDate.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class ParseDate {
 
@@ -12,17 +14,29 @@ public class ParseDate {
     }
 
     public static LocalDate parseFullTextFormat(String stringDate) {
-        if (stringDate == null || stringDate.isEmpty()) {
-            return null;
-        }
-        return LocalDate.parse(stringDate);
+        String[] parts = stringDate.split(" ");
+        String dayOfWeek = parts[0].substring(0, 1).toUpperCase() + parts[0].substring(1).toLowerCase();
+        String day = parts[1];
+        String month = parts[2].substring(0, 1).toUpperCase() + parts[2].substring(1).toLowerCase();
+        String year = parts[3];
+        String normalized = String.format("%s %s %s", day, month, year);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.ENGLISH);
+        return LocalDate.parse(normalized, formatter);
     }
 
     public static LocalTime parseTimeFormat(String stringDate) {
-        if (stringDate == null || stringDate.isEmpty()) {
-            return null;
+        int hour = Integer.parseInt(stringDate.substring(0, 2));
+        if (stringDate.contains("evening")) {
+            if (hour < 12)
+                hour += 12;
         }
-        return LocalTime.parse(stringDate);
+
+        String[] timeParts = stringDate.split(", ");
+        int minute = Integer.parseInt(timeParts[1].split(" ")[0]);
+        int second = Integer.parseInt(timeParts[1].split("and ")[1].split(" ")[0]);
+
+        return LocalTime.of(hour, minute, second);
     }
 
 }
