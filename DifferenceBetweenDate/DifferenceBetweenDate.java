db89143dd55dffcbd62bc8559c3e80969d1a7a29
meljanat <mdjanati.mj@gmail.com>
2025-10-07 15:25:19 +0100

    DifferenceBetweenDate

--- DifferenceBetweenDate/DifferenceBetweenDate.java
+++ DifferenceBetweenDate/DifferenceBetweenDate.java
@@ -7,10 +7,16 @@ import java.time.Period;
 public class DifferenceBetweenDate {
 
     public static Duration durationBetweenTime(LocalTime localTime1, LocalTime localTime2) {
+        if (localTime1 == null || localTime2 == null) {
+            return Duration.ZERO;
+        }
         return Duration.between(localTime1, localTime2).abs();
     }
 
     public static Period periodBetweenDate(LocalDate date1, LocalDate date2) {
+        if (date1 == null || date2 == null) {
+            return Period.ZERO;
+        }
         Period period = Period.between(date1, date2);
         if (period.isNegative()) {
             period = period.negated();
@@ -19,6 +25,9 @@ public class DifferenceBetweenDate {
     }
 
     public static Long numberOfHoursBetweenDateTime(LocalDateTime dateTime1, LocalDateTime dateTime2) {
+        if (dateTime1 == null || dateTime2 == null) {
+            return 0L;
+        }
         return Duration.between(dateTime1, dateTime2).abs().toHours();
     }
 
