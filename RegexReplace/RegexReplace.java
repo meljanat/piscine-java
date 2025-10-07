3bf0884fa7498c743054633fe8e4d8f40e12d222
meljanat <mdjanati.mj@gmail.com>
2025-10-07 14:55:19 +0100

    RegexReplace

---
+++ RegexReplace/RegexReplace.java
@@ -0,0 +1,34 @@
+public class RegexReplace {
+    public static String removeUnits(String s) {
+        return s.replaceAll("\\d+(cm|€)\\s*", "");
+    }
+
+    public static String obfuscateEmail(String s) {
+        int atIdx = s.indexOf('@');
+        if (atIdx == -1)
+            return s;
+
+        String username = s.substring(0, atIdx);
+        String domain = s.substring(atIdx + 1);
+
+        String obfUser = username;
+        if (username.matches(".*[-._].*")) {
+            obfUser = username.replaceAll("(?<=[-._]).", "*").replaceAll("(?<=.)(?=[-._])", "*");
+        } else if (username.length() > 3) {
+            obfUser = username.substring(0, username.length() - 3) + "***";
+        }
+
+        String[] domainParts = domain.split("\\.");
+        String obfDomain = domain;
+        if (domainParts.length == 3) {
+            obfDomain = "*" + "." + domainParts[1] + ".*";
+        } else if (domainParts.length == 2) {
+            String tld = domainParts[1];
+            if (!tld.equals("com") && !tld.equals("org") && !tld.equals("net")) {
+                obfDomain = "*.*";
+            }
+        }
+
+        return obfUser + "@" + obfDomain;
+    }
+}
