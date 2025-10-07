5c8576f79dada1edfea857450b795662dc21e235
meljanat <mdjanati.mj@gmail.com>
2025-10-07 12:01:22 +0100

    ParseDate

--- ParseDate/ParseDate.java
+++ ParseDate/ParseDate.java
@@ -5,14 +5,23 @@ import java.time.LocalTime;
 public class ParseDate {
 
     public static LocalDateTime parseIsoFormat(String stringDate) {
+        if (stringDate == null || stringDate.isEmpty()) {
+            return null;
+        }
         return LocalDateTime.parse(stringDate);
     }
 
     public static LocalDate parseFullTextFormat(String stringDate) {
+        if (stringDate == null || stringDate.isEmpty()) {
+            return null;
+        }
         return LocalDate.parse(stringDate);
     }
 
     public static LocalTime parseTimeFormat(String stringDate) {
+        if (stringDate == null || stringDate.isEmpty()) {
+            return null;
+        }
         return LocalTime.parse(stringDate);
     }
 
