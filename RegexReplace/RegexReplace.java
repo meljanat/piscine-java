fb3caac9e88b23d52b73f20b1341f572135bd68a
meljanat <mdjanati.mj@gmail.com>
2025-10-07 15:15:44 +0100

    RegexReplace

--- RegexReplace/RegexReplace.java
+++ RegexReplace/RegexReplace.java
@@ -1,34 +1,54 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.List;
+
 public class RegexReplace {
-    public static String removeUnits(String s) {
-        return s.replaceAll("(\\d+)(cm|€)( )", "$1$3");
+
+    public static String removeUnits(String input) {
+        return input.replaceAll("(\\d+)(cm|€)(?=\\s|$)", "$1");
     }
 
-    public static String obfuscateEmail(String s) {
-        int atIdx = s.indexOf('@');
-        if (atIdx == -1)
-            return s;
+    public static String obfuscateEmail(String email) {
+        String[] emailParts = email.split("@");
+        if (emailParts.length != 2) {
+            return email;
+        }
+        String user = emailParts[0];
+        String domain = emailParts[1];
 
-        String username = s.substring(0, atIdx);
-        String domain = s.substring(atIdx + 1);
-
-        String obfUser = username;
-        if (username.matches(".*[-._].*")) {
-            obfUser = username.replaceAll("(?<=[-._]).", "*").replaceAll("(?<=.)(?=[-._])", "*");
-        } else if (username.length() > 3) {
-            obfUser = username.substring(0, username.length() - 3) + "***";
+        String obfuscatedUser;
+        Pattern dashDotUnderscore = Pattern.compile(".*[-._]");
+        Matcher matcher = dashDotUnderscore.matcher(user);
+        if (matcher.find()) {
+            int lastSpecialIdx = user.lastIndexOf(user.substring(matcher.end() - 1));
+            String userPrefix = user.substring(0, lastSpecialIdx + 1);
+            String userSuffix = user.substring(lastSpecialIdx + 1);
+            obfuscatedUser = userPrefix + "*".repeat(userSuffix.length());
+        } else if (user.length() > 3) {
+            obfuscatedUser = user.substring(0, 3) + "*".repeat(user.length() - 3);
+        } else {
+            obfuscatedUser = user;
         }
 
+        String obfuscatedDomain;
         String[] domainParts = domain.split("\\.");
-        String obfDomain = domain;
+        List<String> safeTlds = Arrays.asList("com", "org", "net");
+
         if (domainParts.length == 3) {
-            obfDomain = "*" + "." + domainParts[1] + ".*";
+            obfuscatedDomain = "*".repeat(domainParts[0].length()) + "." + domainParts[1] + "."
+                    + "*".repeat(domainParts[2].length());
         } else if (domainParts.length == 2) {
-            String tld = domainParts[1];
-            if (!tld.equals("com") && !tld.equals("org") && !tld.equals("net")) {
-                obfDomain = "*.*";
+            String secondLevel = "*".repeat(domainParts[0].length());
+            String topLevel = domainParts[1];
+            if (!safeTlds.contains(topLevel)) {
+                topLevel = "*".repeat(topLevel.length());
             }
+            obfuscatedDomain = secondLevel + "." + topLevel;
+        } else {
+            obfuscatedDomain = "*".repeat(domain.length());
         }
 
-        return obfUser + "@" + obfDomain;
+        return obfuscatedUser + "@" + obfuscatedDomain;
     }
 }
